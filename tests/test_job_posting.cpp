d7ad6be3eb76bd57b909c35f76202ff01e5a2536
Rolando <rolandoturcios2003@gmail.com>
2026-07-05 20:01:26 -0400

    Adding Testing for job_posting.cpp/.hpp

---
+++ tests/test_job_posting.cpp
@@ -0,0 +1,52 @@
+#include <catch2/catch_test_macros.hpp>
+#include "job_posting.hpp"
+
+TEST_CASE("Job Posting: Intake") {
+	 JobPosting JB{
+		 .firm = "Goldman Sachs",
+		 .external_id = "123",
+		 .title = "Software Engineer",
+		 .location = "New York",
+		 .url = "test.com",
+		 .posted_at = "2026-05-12",
+	 };
+
+	 REQUIRE(JB.firm == "Goldman Sachs");
+	 REQUIRE(JB.external_id == "123");
+	 REQUIRE(JB.title == "Software Engineer");
+	 REQUIRE(JB.location == "New York");
+	 REQUIRE(JB.url == "test.com");
+	 REQUIRE(JB.posted_at == "2026-05-12");
+}
+TEST_CASE("Job Posting: CleanUp") {
+	ScrapeResult SR = {
+		.firm = "Goldman Sachs",
+		.jobs = {
+		JobPosting{
+		.firm = "Goldman Sachs",
+		.external_id = "123",
+		.title = "Software Engineer",
+		.location = "New York",
+		.url = "test.com",
+		.posted_at = "2026-05-12",
+		},
+		JobPosting{
+		.firm = "JP Morgan",
+		.external_id = "456",
+		.title = "",
+		.location = "Connecticut",
+		.url = "test2.com",
+		.posted_at = "2026-06-12",
+		},
+		JobPosting{
+		.firm = "Morgan Stanley",
+		.external_id = "789",
+		.title = "AI Engineer",
+		.location = "Arizona",
+		.url = "test3.com",
+		.posted_at = "2026-07-12",
+		}
+		}
+	};
+	REQUIRE(SR.jobs.size() == 2);
+}
